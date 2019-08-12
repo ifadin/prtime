d7d67d9f940687263bbb5201f5134b349feb952e
Iaroslav Fadin <iaroslav.fadin@zalando.de>
2019-08-12 18:23:43 +0200

    Made time analysis based on dataframes

--- cli/distribution.py
+++ cli/distribution.py
@@ -1,34 +1,32 @@
-import math
 from datetime import datetime, timedelta
 from typing import List
 
 import click
 from github import Github
-from github.Repository import Repository
 from pandas import DataFrame
 
 
-def analyze_repos(gh: Github, repos: List[str], last_days: int, verbose: bool):
+def get_time_analysis(gh: Github, repos: List[str], last_days: int, verbose: bool) -> DataFrame:
+    df = get_time_data(gh, repos, last_days)
+    if verbose:
+        click.echo(df.sort_values('Time', ascending=False))
+
+    count = df.groupby('repo')['Time'].count()
+    mean = df.groupby('repo')['Time'].mean()
+    sem = df.groupby('repo')['Time'].sem()
+
+    return DataFrame({'PRs': count, 'mean': mean, 'sem': sem, 'window_left': mean - sem, 'window_right': mean + sem})
+
+
+def get_time_data(gh: Github, repos: List[str], last_days: int) -> DataFrame:
+    items = []
     for repo in repos:
-        pulls = calculate_pr_time(gh.get_repo(repo), last_days)
-        if not pulls.empty:
-            if verbose:
-                click.echo(pulls.sort_values('Time', ascending=False))
-            time_col = pulls.loc[:, 'Time']
-            mean = time_col.mean()
-            std_err = time_col.std() / math.sqrt(len(time_col))
-
-            click.echo(f'{repo} ({len(time_col)} PRs): '
-                       f'mean={mean:.2f}, std_e={std_err:.2f}, window=[{(mean - std_err):.2f}, {(mean + std_err):.2f}]')
-        else:
+        pulls = gh.get_repo(repo).get_pulls(state='closed')
+        if not pulls:
             click.echo(f'Could not find any matching pull requests in \'{repo}\'')
+        for pr in pulls:
+            if ((pr.created_at >= datetime.today() - timedelta(days=last_days)) if last_days else True) and pr.merged:
+                pr_time = (pr.merged_at - pr.created_at).total_seconds() / 3600
+                items.append((repo, pr.title, pr_time))
 
-
-def calculate_pr_time(repo: Repository, last_days: int) -> DataFrame:
-    return DataFrame(list(
-        {p.title: (p.merged_at - p.created_at).seconds / 3600
-         for p in repo.get_pulls(state='closed')
-         if ((p.created_at >= datetime.today() - timedelta(days=last_days)) if last_days else True) and p.merged
-         }.items()),
-        columns=['PR', 'Time']
-    )
+    return DataFrame(items, columns=['repo', 'PR', 'Time'])
